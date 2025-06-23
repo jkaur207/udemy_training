2878a0c57325092129dc2c052035febdcf490fb8
jkaur207 <jkaur207@rrc.ca>
2025-06-22 19:04:58 -0500

    Add default latest archive page

---
+++ Section-5/app/content/archive/@latest/default.js
@@ -0,0 +1,14 @@
+import { getLatestNews } from "@/lib/news";
+import NewsList from "@/components/news-list";
+
+
+export default async function LatestNewsPage(){
+    const latestNews = await getLatestNews();
+
+    return(
+        <>
+            <h2>Latest News</h2>
+            <NewsList news={latestNews} />
+        </>   
+    );
+}
